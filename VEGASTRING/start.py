361da726ec1e867815d420f5607f99d20155fd75
ZERO <105891882+ZER0X77@users.noreply.github.com>
2024-06-11 03:29:22 +0300

    Add files via upload

--- VEGASTRING/start.py
+++ VEGASTRING/start.py
@@ -29,15 +29,29 @@ from config import OWNER_ID
 # load_dotenv()
 
 
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
 def filter(cmd: str):
     return filters.private & filters.incoming & filters.command(cmd)
 
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
-    await message.reply_photo(
-        photo="https://telegra.ph/file/ce276acf3d3895a712914.jpg",
-        caption=f"<b>⭓ᴍᴜˢɪᴄ✘ᴠᴇɢᴀ♪\n╮⦿ مرحباً بك عزيزي : {msg.from_user.first_name}\n╯⦿ اسمي : {me2}\n╮⦿ تم صنعي من قبل فـيـجا\n╯⦿ اعمل علي استخراج جلسات</b>",
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""𝖧𝐞𝐲 {msg.from_user.mention},
+
+𝖨 𝖠𝐦 {me2},
+𝖳𝖱𝖴𝖲𝖳𝖤𝖣 𝖲𝖳𝖱𝖨𝖭𝖦 𝖦𝖤𝖭𝖤𝖱𝖠𝖳𝖮𝖱 𝖡𝖮𝖳.
+𝖥𝖴𝖫𝖫𝖸 𝖲𝖠𝖥𝖤 & 𝖲𝖤𝖢𝖴𝖱𝖤.
+𝖭𝖮  𝖤𝖱𝖱𝖮𝖱.
+
+𝖬𝐚𝐝𝐞 𝖡𝐲  : [ZERO](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -48,13 +62,10 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton("Qᴜʀᴀɴ", url="https://t.me/QURANI_C")
                 ]
             ]
-        )
+        ),
+        disable_web_page_preview=True,
     )
-
-
-
-
-
+    
 # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 # ▒▒▒▗▉████████████▅▒▒▒▒▒▗██████████▄▒▒▒▄█████████▖▒▒▒▒▒▒▒▒▒▅████████▄▒▒▒▒▒▒▒▒▒▒
 # ▒▒▒▝██████████████▒▒▒▒▒███████████▀▒▒▒███▒▒▒▒▒▒███▒▒▒▒▒▒▒██▘▒▒▒▒▒▒▝██▒▒▒▒▒▒▒▒
